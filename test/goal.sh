5c44540612f2dd916c51a6b76c21030f6314b32b
nuc-bt <felicita64@gmail.com>
2024-05-23 13:36:27 +0900

    feat: goal.sh add

---
+++ test/goal.sh
@@ -0,0 +1,22 @@
+ros2 action send_goal /route_to_pose route_msgs/action/RouteToPose "start_node:
+  node_id: ''
+  position:
+    latitude: 36.113894900000034
+    longitude: 128.36797900008798
+  type: ''
+  kind: 'connecting'
+  heading: 89
+  direction: ''
+  driving_option: 'gps'
+  detection_range: []
+end_node:
+  node_id: ''
+  position:
+    latitude: 36.113894900000034
+    longitude: 128.36736412439808
+  type: ''
+  kind: 'connecting'
+  heading: 140
+  direction: ''
+  driving_option: 'gps'
+  detection_range: []" 
