d492a28be77defe6893b1a20fe01384dc37c9ef4
nuc-bt <felicita64@gmail.com>
2024-05-23 13:15:50 +0900

    chore : goal script add

--- test/goal.sh
+++ test/goal.sh
@@ -1,19 +1,19 @@
 ros2 action send_goal /route_to_pose route_msgs/action/RouteToPose "start_node:
   node_id: ''
   position:
-    latitude: 36.11371630000005
-    longitude: 128.368213
+    latitude: 36.113894900000034
+    longitude: 128.36797900008798
   type: ''
   kind: 'connecting'
-  heading: 8
+  heading: 89
   direction: ''
   driving_option: 'gps'
   detection_range: []
 end_node:
   node_id: ''
   position:
-    latitude: 36.11389810000103
-    longitude: 128.368213
+    latitude: 36.113894900000034
+    longitude: 128.36736412439808
   type: ''
   kind: 'connecting'
   heading: 140
